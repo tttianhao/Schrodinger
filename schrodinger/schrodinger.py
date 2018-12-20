ae48c06e0f177761716c7c7be0f229b80e00d29c
Tianhao Yu <tyu14@u.rochester.edu>
2018-12-19 19:19:08 -0500

    program now takes domain input

--- schrodinger/schrodinger.py
+++ schrodinger/schrodinger.py
@@ -12,6 +12,7 @@ def getParser():
     parser.add_argument('--c', type = float, default = 1, help = 'constant c, default = 1' )
     parser.add_argument('--size', type = int, default = 5, help = 'size of the basis set, default = 5' )
     parser.add_argument('--file', type = str, default = 'schrodinger/potential_energy.dat', help = 'name of your potential energy file')
+    parser.add_argument('--domain', type = str, default = None, help = 'domain, an optional parameter, default = the domain of potential energy data' )
     args = parser.parse_args()
     return args
 
@@ -36,8 +37,8 @@ def read_file(name):
     name: String, file name of the potential energy of the data file
 
     returns:
-    potential: tensor, potential energy input
-    position: tensor, position input '''
+    potential: list, potential energy input
+    position: list, position input '''
     f = open(name, "r")
     potential = []
     position = []
@@ -46,8 +47,6 @@ def read_file(name):
             position.append(float(line.split()[0]))
             potential.append(float(line.split()[1]))
     f.close()
-    potential = tf.constant(potential, dtype = tf.float32)
-    position = tf.constant(position, dtype = tf.float32)
     return potential, position
 
 def inner_V0_b(potential, position, fourier):
@@ -151,29 +150,62 @@ def combine(V0, coeff, n):
     H = coeff + H
     return H
 
+def check_domain(potential, position, domain):
+    ''' This function trims the position/potential data file to fit the domain input
+
+    Args:
+    potential: list, all of the potential energy data
+    position: list, all of the position data
+    domain: string, user input domain 'low,high'
+
+    returns:
+    potential: list, potential data within the domain
+    position: lsit, positions data within the domain
+    '''
+    low = float(domain.split(',')[0])
+    high = float(domain.split(',')[1])
+    if low < position[0] or high > position[-1] or low > high:
+        raise ValueError('Check your domain input. It should be strickly within the range of position data')
+    new_V0 = []
+    new_x = []
+    for i in range(len(position)):
+        if position[i] > low and position[i] < high:
+            new_x.append(position[i])
+            new_V0.append(potential[i])
+    return new_V0, new_x
+
 def main():
     '''
     Main function of the project
     '''
     args = getParser()
+    # Construct basis set
     basis = []
     for i in range(args.size):
         basis.append(fourier(i))
+    # read potential, position data from file
     potential, position = read_file(args.file)
-    print('positions are ',position)
-    print('potentials are ',potential)
+    # check if domain applies
+    if args.domain != None:
+        potential, position = check_domain(potential, position, args.domain)
+    potential = tf.constant(potential, dtype = tf.float32)
+    position = tf.constant(position, dtype = tf.float32)
+    # calculate <V0,b>
     integration = inner_V0_b(potential,position,basis)
     integration = tf.reshape(integration,[args.size,1])
+    # calculate <V0hat, b>
     coefficient = coefficient_matrix(basis,position)
-    #print(coefficient)
+    # project V0 onto basis
     solution = tf.linalg.solve(coefficient, integration)
+    # project the first half of Hamltonian to basis
     matrix = operator(args.c,args.size)
+    # Combine two halves of the Hamltonian
     H = combine(solution,matrix,args.size)
-    print('Hamiltonian is :',H)
+    # solve the eigenvector eigenvalue problem
     e,v = tf.linalg.eigh(H)
     print('Lowest energy state of the Hamiltonian: ',e[0])
     print("The wavefunction's coefficient on the basis set: ",v[0])
-    return True
+    return H, e[0], v[0]
 
 if __name__ == '__main__':
     main()
